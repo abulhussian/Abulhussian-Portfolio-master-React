52a2a9dd1c44f0c20cba989669c1faf2e034b832
abulhussian12 <abulhussian1229@gmail.com>
2025-01-05 10:00:40 +0530

    fist commit

---
+++ src/components/skills.js
@@ -0,0 +1,100 @@
+import React from "react";
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import Javascript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Github from "../assets/github.png";
+import Tailwind from "../assets/tailwind.png";
+import Python from "../assets/python.jpeg";
+import Selenium from "../assets/selenium.png";
+import sql from "../assets/sql.png";
+
+const Skills = () => {
+  return (
+    <div className="pt-52 bg-[#0a192f]">
+      <div
+        name="skills"
+        className="bg-[#0a192f] text-gray-300 w-full sm:h-screen sm:pt-0"
+      >
+        {/* Container */}
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center h-full w-full">
+          <div>
+            <p className="text-4xl font-bold inline border-b-4 border-[#ccd6f6]">
+              Skills
+            </p>
+            <p className="py-4"> These are the technologies I've worked with</p>
+          </div>
+
+          {/* Core Skills Section */}
+          <div className="mt-8">
+            <p className="text-2xl font-bold text-[#ccd6f6]">Core Skills</p>
+            <div className="w-4/5 grid grid-cols-2 sm:grid-cols-3 mx-auto gap-5 text-center py-8">
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={HTML} alt="html icon" />
+                <p className="my-4">HTML</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={CSS} alt="css icon" />
+                <p className="my-4">CSS</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Javascript} alt="js icon" />
+                <p className="my-4">Javascript</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={ReactImg} alt="react icon" />
+                <p className="my-4">ReactJS</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Github} alt="Github icon" />
+                <p className="my-4">Github</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500">
+                <img
+                  className="w-20 mx-auto"
+                  src={Tailwind}
+                  alt="Tailwind icon"
+                />
+                <p className="my-4">Tailwind</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Divider */}
+          <div className="w-4/5 mx-auto border-t-2 border-gray-500 my-8"></div>
+
+          {/* Additional Skills Section */}
+          <div className="mt-8 pb-80">
+            <p className="text-2xl font-bold text-[#ccd6f6]">
+              Additional Knowledge
+            </p>
+            <p className="py-4">
+              These are some additional tools and technologies I have basic
+              knowledge of:
+            </p>
+            <div className="w-4/5 grid grid-cols-2 sm:grid-cols-3 mx-auto gap-5 text-center py-8">
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500 pt-5">
+                <img className="w-20 mx-auto" src={Python} alt="Python icon" />
+                <p className="my-4">Python</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500 pt-5">
+                <img
+                  className="w-20 mx-auto"
+                  src={Selenium}
+                  alt="Selenium icon"
+                />
+                <p className="my-4">Selenium (Automation Testing)</p>
+              </div>
+              <div className="shadow-md py-2 shadow-[#040c16] hover:scale-110 duration-500  pt-5">
+                <img className="w-20 mx-auto " src={sql} alt="SQL icon" />
+                <p className="my-4">SQL</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
---
+++ src/components/work.js
@@ -0,0 +1,186 @@
+import React from "react";
+import Bookmark from "../assets/bookmark_smr.webp";
+import ProductList from "../assets/product_list_cart.webp";
+import ecommerce from "../assets/ecommerce.webp";
+import Furniro from "../assets/furniro.png";
+import Tipcalculator from "../assets/tip-calculator.webp";
+import Travel from "../assets/travel.jpg";
+
+const Work = () => {
+  return (
+    <div className="pt-52 bg-[#0a192f] ">
+      <div
+        name="work"
+        className="sm:pt-0 pt-12 md:h-screen text-gray-300 bg-[#0a192f] "
+      >
+        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full  h-full">
+          <div className="pb-8">
+            <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#ccd6f6]">
+              Work
+            </p>
+            <p className="py-6 ">Check out some of my recent work</p>
+          </div>
+
+          {/*    Container       */}
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+            {/*        Grid Item 1        */}
+            <div
+              style={{ backgroundImage: `url(${Bookmark})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  JS + TailwindCSS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://bookmark-manager-smr.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/syedmujeeb123/Bookmark-Manager">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/*        Grid Item  2      */}
+            <div
+              style={{ backgroundImage: `url(${ProductList})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  JS + TailwindCSS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://prduct-list-jason-js-08.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/abulhussian/product-list-Jason-08">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/*        Grid Item    3    */}
+            <div
+              style={{ backgroundImage: `url(${ecommerce})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  JS + TailwindCSS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://e-commerence-product-list-js-10.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/abulhussian/e-commerence-product-10">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/*        Grid Item   4     */}
+            <div
+              style={{ backgroundImage: `url(${Furniro})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  ReactJS + TailwindCSS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://furniro-dusky.vercel.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/syedmujeeb123/Furniro">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/*        Grid Item    5    */}
+            <div
+              style={{ backgroundImage: `url(${Tipcalculator})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  ReactJS + TailwindCSS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://vercel.com/abulhussians-projects/tip-calculator-redux-toolkit-02">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/abulhussian/Tip-Calculator-redux-toolkit-02">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            {/*        Grid Item   6      */}
+            <div
+              style={{ backgroundImage: `url(${Travel})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            >
+              {/* Hover effects */}
+
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  ReactJS
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://travel-list-react-js-01.netlify.app/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a href="https://github.com/abulhussian/Travel-List-React-js-01">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
